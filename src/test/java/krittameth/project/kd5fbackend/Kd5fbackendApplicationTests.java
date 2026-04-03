c1fe1440524db838f0ff8b7c2786b3a0c08d2502
GERRY Express <krittameth2020@gmail.com>
2026-04-03 10:19:03 +0700

    initalize project

---
+++ src/test/java/krittameth/project/kd5fbackend/Kd5fbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package krittameth.project.kd5fbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kd5fbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
