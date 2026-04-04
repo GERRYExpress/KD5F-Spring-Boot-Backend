bd20c418ca0337561527f41153ca6d58d3267574
GERRY Express <krittameth2020@gmail.com>
2026-04-04 12:07:20 +0700

    change the getter methods into lombok's annotation for readability and add the productlist object for associative entity

--- src/main/java/krittameth/project/kd5fbackend/model/Category.java
+++ src/main/java/krittameth/project/kd5fbackend/model/Category.java
@@ -1,12 +1,19 @@
 package krittameth.project.kd5fbackend.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import lombok.Getter;
 
 @Entity
+@Getter
 @Table(name = "categories")
 public class Category {
     @Id
@@ -14,23 +21,12 @@ public class Category {
     @Column(name = "category_id")
     private int id;
 
+    @OneToMany(mappedBy = "category", cascade = {CascadeType.MERGE})
+    private List<ProductList> productLists = new ArrayList<>();
+
     @Column(name = "name")
     private String name;
 
     @Column(name = "isHidden")
     private boolean hidden;
-
-
-    public int getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public boolean getHidden() {
-        return hidden;
-    }
-
 }
--- src/main/java/krittameth/project/kd5fbackend/model/Product.java
+++ src/main/java/krittameth/project/kd5fbackend/model/Product.java
@@ -1,12 +1,21 @@
 package krittameth.project.kd5fbackend.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import lombok.Getter;
 
 @Entity
+@Getter
 @Table(name = "products")
 public class Product {
     @Id
@@ -27,23 +36,8 @@ public class Product {
     private String poster_url;
 
 
-    public int getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public String getPoster_url() {
-        return poster_url;
-    }
-
-    public double getPrice() {
-        return price;
-    }
+    // Map to Associative Entity (product_lists)
+    @OneToMany(mappedBy = "product", cascade = {CascadeType.MERGE})
+    @JsonIgnore
+    private List<ProductList> productLists = new ArrayList<>();
 }
