12bd622e1872d937ad162de80c67b39221639e56
GERRY Express <krittameth2020@gmail.com>
2026-04-05 12:10:05 +0700

    change getProduct_Id() to getProduct_id() due to avoid typo in frontend implementation

--- src/main/java/krittameth/project/kd5fbackend/projection/ProductListProjection.java
+++ src/main/java/krittameth/project/kd5fbackend/projection/ProductListProjection.java
@@ -1,7 +1,7 @@
 package krittameth.project.kd5fbackend.projection;
 
 public interface ProductListProjection {
-    Integer getProduct_Id();
+    Integer getProduct_id();
     String getCategory();
     String getName();
     Double getPrice();
